8feb168be64a0c9e345a83c61a2b33c3dc4fe913
hammadalimwt <hammadalimwt@gmail.com>
2026-06-21 23:07:52 +0500

    changes in frntend

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,8 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || '/',
   withCredentials: true,
 });
+console.log(import.meta.env.VITE_API_URL);
+
 
 // Attach token to every request
 api.interceptors.request.use((config) => {
